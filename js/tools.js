95ae4fd8415ebbf7557ada7d65ac441c0c009fce
viebone <viebone@gmail.com>
2017-09-06 23:59:44 +0100

    added javascript for the sections, css and some style refinement

--- js/tools.js
+++ js/tools.js
@@ -4,15 +4,37 @@ var tools = {
 	init : function(){
 		var self = this;
 		
-		self.home();
+		self.singlePageApplicationInit();
 	},
-	home : function(){
+	//spa init function
+	singlePageApplicationInit : function(){
+		//reference to the object
 		var self = this;
-		var homeimage = document.getElementById("section-images__home");
-		console.log("width: " + homeimage.style);
-		homeimage.style.height = (self.h-200) + "px";
-		console.log("height: " + self.h);
+		//class used for each of the single page app section
+		var targetclass = "spa-section";
+		//the images on the home to make it full height
+		var targetID = "section-images__home";
+		//home image
+		var homeimage = document.getElementById(targetID);
+		//array with all sections in the spa
+		var spa_sections = [];
+		//space to substract from the total of the screen size in order to allocate the google fab cta
+		var vmargin = 150;
+		//adding sections to the array
+		spa_sections = document.getElementsByClassName(targetclass);
+		//setting the homeimage height
+		homeimage.style.height = (self.h - vmargin) + "px";
+		//setting the sections height according to screen size minus margin
+		self.setSpaSectionsHeight(spa_sections,self.h - vmargin);
+		console.log(spa_sections);
+	},
+	//function to set the spa sections
+	setSpaSectionsHeight : function(tar,val){
+		for(x=0;x<tar.length;x++){
+			tar[x].style.height = val + "px";
+		}
 	}
+
 
 };
 
