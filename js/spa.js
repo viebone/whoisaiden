f575cde9a7e1e42fe9da696004f8de99dafa7859
viebone <viebone@gmail.com>
2017-09-10 23:16:36 +0100

    refactor code to meet the new spa concept. Added home and template 1 styles

--- js/spa.js
+++ js/spa.js
@@ -1,6 +1,7 @@
-var tools = {
+var spa = {
 	w : window.innerWidth,
 	h : window.innerHeight,
+	gutter : 16,
 	init : function(){
 		var self = this;
 		
@@ -11,21 +12,21 @@ var tools = {
 		//reference to the object
 		var self = this;
 		//class used for each of the single page app section
-		var targetclass = "spa-section";
-		//the images on the home to make it full height
-		var targetID = "section-images__home";
-		//home image
-		var homeimage = document.getElementById(targetID);
+		var spa_target_class = "spa-section";
+		//id of the home section
+		var home_section_id = "spa-section-home";
+		//home section storage variable
+		var spa_home_section = document.getElementById(home_section_id);
 		//array with all sections in the spa
 		var spa_sections = [];
 		//space to substract from the total of the screen size in order to allocate the google fab cta
-		var vmargin = 150;
+		var vmargin = 100;
 		//adding sections to the array
-		spa_sections = document.getElementsByClassName(targetclass);
+		spa_sections = document.getElementsByClassName(spa_target_class);
 		//setting the homeimage height
-		homeimage.style.height = (self.h - vmargin) + "px";
+		spa_home_section.style.height = (self.h - vmargin) + "px";
 		//setting the sections height according to screen size minus margin
-		self.setSpaSectionsHeight(spa_sections,self.h - vmargin);
+		self.setSpaSectionsHeight(spa_sections,self.h-(self.gutter*2));
 		console.log(spa_sections);
 	},
 	//function to set the spa sections
@@ -38,4 +39,4 @@ var tools = {
 
 };
 
-tools.init();
+spa.init();
